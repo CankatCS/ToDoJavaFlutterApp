d551df7ae47b97f01a44ba55dad320713535ec0c
Cankat Sezer <cankat_sezer055@hotmail.com>
2023-03-19 01:27:11 +0300

    Project created

---
+++ src/main/java/com/example/todojavaflutterapp/ToDoJavaFlutterAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.todojavaflutterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoJavaFlutterAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoJavaFlutterAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/todojavaflutterapp/ToDoJavaFlutterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todojavaflutterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoJavaFlutterAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
